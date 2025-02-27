053688844f9b35e29723a2c81a8853f6cfd1fed3
dougzzzz <douglasbarcelos02@gmail.com>
2025-02-27 20:57:16 -0300

    Anotações do swagger incluídas

--- CadastroDeNotasFiscais/Controllers/NotasFiscaisController.cs
+++ CadastroDeNotasFiscais/Controllers/NotasFiscaisController.cs
@@ -1,6 +1,7 @@
 ﻿using CadastroDeNotasFiscais.Dominio.NotasFiscais;
 using CadastroDeNotasFiscais.Serviços;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace CadastroDeNotasFiscais.Controllers
 {
@@ -16,6 +17,9 @@ namespace CadastroDeNotasFiscais.Controllers
         }
 
         [HttpGet]
+        [SwaggerOperation(Summary = "Obter todas as notas fiscais", Description = "Retorna uma lista de notas fiscais, opcionalmente filtrada.")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<NotaFiscal>))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public IActionResult ObterTodos([FromQuery] FiltroDasNotasFiscais filtro = null)
         {
             var listaDeNotasFiscais = _servicoDasNotasFiscais.ObterTodos(filtro);
@@ -24,6 +28,10 @@ namespace CadastroDeNotasFiscais.Controllers
         }
 
         [HttpPost]
+        [SwaggerOperation(Summary = "Adicionar uma nova nota fiscal", Description = "Adiciona uma nova nota fiscal ao sistema.")]
+        [Consumes("application/json")]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(NotaFiscal))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public IActionResult Adicionar([FromBody] NotaFiscal notaFiscal)
         {
             try
@@ -39,6 +47,9 @@ namespace CadastroDeNotasFiscais.Controllers
         }
 
         [HttpGet("{id}")]
+        [SwaggerOperation(Summary = "Obter uma nota fiscal por ID", Description = "Retorna uma nota fiscal específica com base no ID fornecido.")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(NotaFiscal))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult ObterPorId(string id)
         {
             try
