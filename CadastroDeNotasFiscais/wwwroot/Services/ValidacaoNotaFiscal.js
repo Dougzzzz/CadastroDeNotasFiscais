d68a41ee7e58e6a4ae8b48405e68bdfaf1a4ca23
dougzzzz <douglasbarcelos02@gmail.com>
2025-02-26 21:19:01 -0300

    validação incluída

---
+++ CadastroDeNotasFiscais/wwwroot/Services/ValidacaoNotaFiscal.js
@@ -0,0 +1,26 @@
+sap.ui.define([
+], () => {
+    "use strict";
+
+    return {
+        validarCamposObrigatorios: function (modelo, caminhos, recursosI18n) {
+            debugger
+            const erros = caminhos.map(campo => {
+                const valor = modelo.getProperty(campo.caminho);
+                const input = campo.input;
+
+                if (!valor || valor.trim() === "") {
+                    const mensagemErro = recursosI18n.getText(campo.mensagemI18n);
+                    input.setValueState("Error");
+                    input.setValueStateText(mensagemErro);
+                    return mensagemErro;
+                } else {
+                    input.setValueState("None");
+                    return null;
+                }
+            }).filter(mensagem => mensagem !== null);
+
+            return erros;
+        }
+    };
+})
