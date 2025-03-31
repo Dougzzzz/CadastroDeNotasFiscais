24f2d53e86b82847781647ed115f111bdff8e90e
dougzzzz <douglasbarcelos02@gmail.com>
2025-03-31 06:20:46 -0300

    componente filtro atualizado

--- CadastroDeNotasFiscais.Angular/src/app/app.component.ts
+++ CadastroDeNotasFiscais.Angular/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { ListaDeNotasComponent } from './components/lista-de-notas/lista-de-notas.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BarraDeFiltrosComponent } from './components/barra-de-filtros/barra-de-filtros.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true, 
   styleUrl: './app.component.css',
-  imports: [ListaDeNotasComponent]
+  imports: [ListaDeNotasComponent, HomeComponent, RouterModule, BarraDeFiltrosComponent],
 })
 export class AppComponent {
   title = 'CadastroDeNotasFiscais.Angular';
--- CadastroDeNotasFiscais.Angular/src/app/components/barra-de-filtros/barra-de-filtros.component.ts
+++ CadastroDeNotasFiscais.Angular/src/app/components/barra-de-filtros/barra-de-filtros.component.ts
@@ -1,17 +1,31 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatInputModule} from '@angular/material/input';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-barra-de-filtros',
-  standalone: true,
   templateUrl: './barra-de-filtros.component.html',
-  styleUrl: './barra-de-filtros.component.css',
-  imports: [MatExpansionModule, MatFormFieldModule, MatDatepickerModule, MatInputModule],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./barra-de-filtros.component.css'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule
+  ]
 })
 export class BarraDeFiltrosComponent {
-  readonly panelOpenState = signal(false);
+  panelOpenState = signal(false);
 }
