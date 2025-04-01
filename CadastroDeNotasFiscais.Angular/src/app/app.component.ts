8c4c2683a003319a5a4840ced210696d50b88040
dougzzzz <douglasbarcelos02@gmail.com>
2025-04-01 05:53:07 -0300

    alterei a ordem dos componentes importando a lista e a barra de filtros para a home

--- CadastroDeNotasFiscais.Angular/src/app/app.component.ts
+++ CadastroDeNotasFiscais.Angular/src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
-import { ListaDeNotasComponent } from './components/lista-de-notas/lista-de-notas.component';
 import { HomeComponent } from './pages/home/home.component';
-import { BarraDeFiltrosComponent } from './components/barra-de-filtros/barra-de-filtros.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true, 
   styleUrl: './app.component.css',
-  imports: [ListaDeNotasComponent, HomeComponent, RouterModule, BarraDeFiltrosComponent],
+  imports: [HomeComponent, RouterModule],
 })
 export class AppComponent {
   title = 'CadastroDeNotasFiscais.Angular';
--- CadastroDeNotasFiscais.Angular/src/app/pages/home/home.component.ts
+++ CadastroDeNotasFiscais.Angular/src/app/pages/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import { BarraDeFiltrosComponent } from '../../components/barra-de-filtros/barra-de-filtros.component';
+import { ListaDeNotasComponent } from '../../components/lista-de-notas/lista-de-notas.component';
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  imports: [BarraDeFiltrosComponent, ListaDeNotasComponent]
 })
 export class HomeComponent {
 
