543d1f53f287b995bcc709cb3f5394c4365850ae
dougzzzz <douglasbarcelos02@gmail.com>
2025-04-15 06:16:24 -0300

    configuração de inicialização do projeto angular

--- CadastroDeNotasFiscais/Program.cs
+++ CadastroDeNotasFiscais/Program.cs
@@ -4,6 +4,16 @@ using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AngularPolicy",
+        builder => builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
 ModuloDeInjecaoDeDependencia.AdicionarServicos(builder.Services, builder.Configuration);
 
 var app = builder.Build();
@@ -18,6 +28,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseDefaultFiles();
+
 app.UseStaticFiles(new StaticFileOptions
 {
     FileProvider = new PhysicalFileProvider(
@@ -30,8 +41,52 @@ app.UseStaticFiles(new StaticFileOptions
     }
 });
 
+string angularDistPath = Path.Combine(
+    Directory.GetParent(Directory.GetCurrentDirectory()).FullName,
+    "CadastroDeNotasFiscais.Angular",
+    "dist",
+    "cadastro-de-notas-fiscais.angular"
+);
+
+if (Directory.Exists(angularDistPath))
+{
+    app.UseStaticFiles(new StaticFileOptions
+    {
+        FileProvider = new PhysicalFileProvider(angularDistPath),
+        RequestPath = "/angular"
+    });
+
+
+    // Configurar fallback para SPA Angular
+    app.MapWhen(
+        context => context.Request.Path.StartsWithSegments("/angular"),
+        appBuilder =>
+        {
+            appBuilder.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(angularDistPath),
+                RequestPath = ""
+            });
+
+            appBuilder.Run(async context =>
+            {
+                context.Response.ContentType = "text/html";
+                await context.Response.SendFileAsync(Path.Combine(angularDistPath, "index.html"));
+            });
+        }
+    );
+}
+else
+{
+    Console.WriteLine($"AVISO: Pasta Angular dist não encontrada em: {angularDistPath}");
+}
+
+app.UseCors("AngularPolicy");
+
 app.UseAuthorization();
 
 app.MapControllers();
+
+
 
 app.Run();
